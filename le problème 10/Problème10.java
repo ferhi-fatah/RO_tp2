97f62193977e816d11471ae37c43e5962f1f568f
ferhi-fatah <101930870+ferhi-fatah@users.noreply.github.com>
2022-04-09 21:55:55 +0100

    Add files via upload

---
+++ le problème 10/Problème10.java
@@ -0,0 +1,13 @@
+import ilog.concert.*;
+import ilog.cplex.*;
+public class Problème10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 calcul ();
+	}
+		 public static void calcul (){
+
+	}
+
+}
